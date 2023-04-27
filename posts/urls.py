7577c8dc5253b23151b75e039c33ab6a7966fdb2
Park <angle307@naver.com>
2023-04-27 17:23:32 +0900

    [ADD] Post create/detail

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/', views.detail, name='detail'),
 ]
