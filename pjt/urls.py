e85157e5f8687a8c279b6d02cc122528342245ba
Park <angle307@naver.com>
2023-04-27 14:28:53 +0900

    [ADD] index

--- pjt/urls.py
+++ pjt/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
+app_name = 'trip'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
