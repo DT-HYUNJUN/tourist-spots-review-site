f4105fd3435d62a76f86ae75c8ec825abe8396b3
Park <angle307@naver.com>
2023-04-27 23:57:19 +0900

    [ADD] Article model

--- posts/models.py
+++ posts/models.py
@@ -9,13 +9,13 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(blank=True, upload_to='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
     
     def post_image_path(instance, filename):
         return f'posts/{instance.pk}/{filename}'
+    image = models.ImageField(blank=True, upload_to=post_image_path)
     
     def delete(self, *args, **kargs):
         if self.image:
@@ -47,7 +47,7 @@ class Post(models.Model):
             return self.strftime('%Y-%m-%d')
 
 
-class Comment(models.Model):
+class PostComment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
