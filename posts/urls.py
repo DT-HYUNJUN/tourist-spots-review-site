5f6cafb6790b26be649a6797b502ba4053fb41ba
Park <angle307@naver.com>
2023-04-28 10:19:50 +0900

    [UPDATE] urls

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('<int:post_pk>/', views.detail, name='detail'),
-    path('<int:post_pk>/delete', views.delete, name='delete'),
-    path('<int:post_pk>/update', views.update, name='update'),
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path('<int:post_pk>/update/', views.update, name='update'),
 ]
