5816fde5a53ea4377cc9ad141b65f1cdb60a343e
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-05-04 16:51:18 +0900

    article-Update-TagMutiImageFixed

--- static/js/accounts-profile.js
+++ static/js/accounts-profile.js
@@ -13,9 +13,9 @@ form.addEventListener('submit', function (event) {
     const isFollowed = response.data.is_followed
     const followBtn = document.querySelector('#follow-form > input[type=submit]')
     if (isFollowed === true) {
-      followBtn.value = '언팔로우'
+      followBtn.value = 'Unfollow'
     } else {
-      followBtn.value = '팔로우'      
+      followBtn.value = 'Follow'      
     }
     const followingCountTag = document.querySelector('#followings-count')
     const followerCountTag = document.querySelector('#followers-count')
